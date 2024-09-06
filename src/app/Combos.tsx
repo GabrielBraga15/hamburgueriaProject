1f3a8b79c02d4fde3d4226b2c498e8bdfebd9b0d
Gabriel Braga <gabrielferreira9034@gmail.com>
2024-09-06 12:43:15 -0300

    combos

--- src/app/Combos.tsx
+++ src/app/Combos.tsx
@@ -43,7 +43,7 @@ export default function Combos() {
         <div className="flex justify-between items-center mb-4">
           <div className="flex-1 px-4 text-left">
             <h3 className="text-2xl font-bold text-black">Combo X-BACON</h3>
-            <p className="text-xl text-black">2 X-BACON e 1 REFRIGERANTE 1.5L</p>
+            <p className="text-xl text-black">2 X-BACON E 1 REFRIGERANTE 1.5L</p>
             <p className="text-xl font-bold text-black">R$ 35,99</p>
           </div>
           <div className="w-24 h-24 bg-amber-300 rounded-lg"></div> {/* Placeholder para imagem */}
